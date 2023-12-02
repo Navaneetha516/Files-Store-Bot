4d16523b499a3825f7e4b77d81543aee83cfc37c
Navaneetha516 <121594658+Navaneetha516@users.noreply.github.com>
2023-12-02 17:54:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002038499515"))
 OWNER_ID = int(os.environ.get("OWNER_ID", "1439022100"))
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://MovieTimeHD1:MovieTimeHD1@cluster0.trfqzij.mongodb.net/")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://MovieTimeHD1:MovieTimeHD1@cluster0.trfqzij.mongodb.net")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001688675040"))
